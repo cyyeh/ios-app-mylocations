9808f9be66328d921fbf18b6b282efff64c232aa
cyyeh <chihyu.jimmy.yeh@gmail.com>
2019-04-16 11:14:59 +0800

    get location data

---
+++ MyLocations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by ChihYu Yeh on 2019/4/16.
+//  Copyright © 2019 cyyeh. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
